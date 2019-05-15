a9723fb3c40f52cb93a35beb1f582e136d6da1e7
iwh718 <630020826@qq.com>
2019-05-15 09:22:40 +0800

    增加JSON数据返回

--- index.php
+++ index.php
@@ -1,10 +1,40 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2019/3/30
- * Time: 10:48
- */
-require "GetWangYiYunInfo.php";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>倾听文字的声音</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="res/css/index.css">
+</head>
+<body>
+<div class="btn">网易云每日歌曲热评
+    <div class="btn2"></div>
+</div>
 
-$res = new GetWangYiYunInfo();
+<p>倾听文字的声音 | <?php  echo  Date("Y/m/d")?></p>
+<ul id='timeline'>
+        <?php
+        require('./wyy.php');
+            $g = new GetWangYiYunInfo();
+            $res =  $g->returnData();
+
+        foreach ($res as $row) {
+            echo "<li class='work'>";
+            echo "<p>歌曲名:{$row['name']}</p>";
+            echo "<p>By:{$row['user']}</p>";
+            echo " <div class=\"relative\">";
+            echo "    <span class='date'>日期：{$row['time']}</span>";
+            echo " <span class='circle'></span>";
+            echo " </div>";
+            echo "<div class='content'>";
+            echo "  <p>{$row['comments']}";
+            echo "</p>";
+            //点赞人数
+          //  echo "<p> <span class='fa fa-thumbs-up'/>{$row['like']} </p>";
+            echo " </div>   </li>";
+        }
+        ?>
+</ul>
+<footer><p>数据来自：网易云音乐 | IWH  @API来自Git</p></footer>
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,16 @@
+网易云评论获取
+支持JSON返回与数组。
+
+引入 wyy.php
+
+  实例化：$G =  GetWangYiYunInfo对象
+  
+  获取JSON：$G->returnJSON();
+  
+  获取数组：$G->returnData();
+  
+  eg：
+  
+  ![预览](./res/imgs/1.png)
+  
+  
